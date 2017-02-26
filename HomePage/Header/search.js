f0f9d0a0f6654be8fdb1511f28da120f40e0ce9c
MasterChief26 <shalom.ellezam@gmail.com>
2017-02-26 16:45:54 +0100

    responsive est fait il reste le menu burger à faire, c'est en cours livrable ce soir

--- HomePage/Header/search.js
+++ HomePage/Header/search.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     $('#datepicker').datepicker();
-    $(function(){
-    $('#datepicker').datepicker({
-      inline: true,
-      showOtherMonths: true,
-      dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    });
-  });
+    $('.check-in').click(function(){
+        $('.tog').toggle();
+    })
+  $(".item").click(function() {
+  $("h4.selected").text($(this).text());
+});
 });
