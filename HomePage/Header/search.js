f33dcc3eb7f46024c90e57dff1ebba1db8b32765
MasterChief26 <shalom.ellezam@gmail.com>
2017-02-27 16:07:50 +0100

    fix probleme header

--- HomePage/Header/search.js
+++ HomePage/Header/search.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     $('#datepicker').datepicker();
+    $('#datepickerImage').click(function() {
+    $("#datepicker").focus();
+  });
     $('.tog').hide();
     $('.check-in').click(function(){
         $('.tog').slideToggle();
-    })
+    });
   $(".item").click(function() {
   $("h4.selected").text($(this).text());
 });
@@ -12,5 +15,5 @@ $(document).ready(function() {
         $('.burger-menu').toggle("slide");
         $('.logo-resp').toggle();
         $('section').toggleClass("active");
-    })
+    });  
 });
