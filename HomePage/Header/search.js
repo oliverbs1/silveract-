ecaa4182235014de25988a6a463c4e42916b9d89
MasterChief26 <shalom.ellezam@gmail.com>
2017-03-10 23:01:38 +0100

    add pop-up-register

--- HomePage/Header/search.js
+++ HomePage/Header/search.js
@@ -19,12 +19,29 @@ $(document).ready(function() {
             'overflow':'hidden'
         })
     }); 
-    
     $('.cross,.cross-line-t').click(function(){
         $('#menu-hamburger').toggle("slide");
         console.log('menu close');
         $('body').css({
             'overflow' : 'scroll'
         })
+    })
+    $('.pop-out').hide();
+    $('.pop-up').hide();
+    $('.pop-in').click(function(){
+        $('.pop-out').fadeToggle();
+        $('.pop-up').fadeToggle();
+    })
+    $('.register-with-mail').click(function(){
+        $('.pop-end').fadeIn();
+        $('.pop-up').hide();
+    })
+    $('.close-pop-end').click(function(){
+        $('.pop-end').fadeOut();
+        $('.pop-out').fadeOut();
+    })
+    $('.close-pop-up').click(function(){
+         $('.pop-up').fadeOut();
+        $('.pop-out').fadeOut();
     })
 });
