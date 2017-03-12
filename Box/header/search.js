58967d05fe4bd5b388062f8da6dc3e517da86420
GaspardFremy <gaspardfremy123@gmail.com>
2017-03-12 16:17:20 +0100

    add video + connect inscription form to all header

--- Box/header/search.js
+++ Box/header/search.js
@@ -3,13 +3,27 @@ $(document).ready(function() {
     $('#datepickerImage').click(function() {
     $("#datepicker").focus();
   });
-    $('.tog').hide();
     $('.check-in').click(function(){
-        $('.tog').slideToggle();
+        $('.pop-out').fadeIn();
+        $('.content-tog').fadeIn();
     });
   $(".item").click(function() {
   $("h4.selected").text($(this).text());
+  $('.pop-out').fadeOut();
+      $('.content-tog').fadeOut();
 });
+    $('.next-arrow i').click(function(){
+        $('.first-selection').hide();
+        $('.second-selection').fadeIn(1000);
+    });
+    $('.prev-arrow i').click(function(){
+        $('.second-selection').hide();
+        $('.first-selection').fadeIn(1000);
+    });
+    $('.close-tog').click(function(){
+        $('.pop-out').fadeOut();
+        $('.content-tog').fadeOut();
+    })
     $('#menu-hamburger').hide();
     
     $('.icon-burger').click(function(){
@@ -19,12 +33,29 @@ $(document).ready(function() {
             'overflow':'hidden'
         })
     }); 
-    
     $('.cross,.cross-line-t').click(function(){
         $('#menu-hamburger').toggle("slide");
         console.log('menu close');
         $('body').css({
             'overflow' : 'scroll'
         })
+    })
+    $('.pop-out').hide();
+    $('.pop-up').hide();
+    $('.pop-in').click(function(){
+        $('.pop-out').fadeToggle();
+        $('.pop-up').fadeToggle();
+    })
+    $('.register-with-mail').click(function(){
+        $('.pop-end').fadeIn();
+        $('.pop-up').hide();
+    })
+    $('.close-pop-end').click(function(){
+        $('.pop-end').fadeOut();
+        $('.pop-out').fadeOut();
+    })
+    $('.close-pop-up').click(function(){
+         $('.pop-up').fadeOut();
+        $('.pop-out').fadeOut();
     })
 });
